3fa17888fd62441d4cb2c5ec7e073cff73ce5aed
ButialRalph <ralphjordanbutial@gmail.com>
2018-05-10 10:13:41 +0800

    change structure to es6

--- Calendar.js
+++ Calendar.js
@@ -1,66 +1,31 @@
-let Calendar = (function() {
-  var days = {
-      'Sun': 'Sunday',
-      'Mon': 'Monday',
-      'Tue': 'Tuesday',
-      'Wed': 'Wednesday',
-      'Thu': 'Thursday',
-      'Fri':'Friday',
-      'Sat': 'Saturday'
-  };
-  var months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December'
-  ];
-  var ordinals = {
-      '1st': 1,
-      '2nd': 2,
-      '3rd': 3,
-      '4th': 4,
-      '5th': 5,
-      '6th': 6,
-      '7th': 7,
-      '8th': 8,
-      '9th': 9,
-      '10th': 10,
-      '11th': 11,
-      '12th': 12,
-      '13th': 13,
-      '14th': 14,
-      '15th': 15,
-      '16th': 16,
-      '17th': 17,
-      '18th': 18,
-      '19th': 19,
-      '20th': 20,
-      '21st': 21,
-      '22nd': 22,
-      '23rd': 23,
-      '24th': 24,
-      '25th': 25,
-      '26th': 26,
-      '27th': 27,
-      '28th': 28,
-      '29th': 29,
-      '30th': 30,
-      '31st': 31,
+class Calendar {
+  constructor() {
+     this.days = {
+        'Sun': 'Sunday',
+        'Mon': 'Monday',
+        'Tue': 'Tuesday',
+        'Wed': 'Wednesday',
+        'Thu': 'Thursday',
+        'Fri':'Friday',
+        'Sat': 'Saturday'
+    };
+    this.months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
   }
-  var month;
-  var year;
-  var day;
-  var date;
-  return {
-    get(param) {
+
+  get(param) {
       let eloquent = ''
       if (param) {
         eloquent = param.split(' ');
@@ -74,21 +39,17 @@ let Calendar = (function() {
           this.year = new Date().getFullYear()
       }
       return this.getDay(this.month, this.day, this.year)
-    },
-    enumerateFirstWord(word) {
+  }
+
+  enumerateFirstWord(word) {
       if (word.toUpperCase() === 'LAST') {
-        if (!this.year) {
-            this.year = new Date().getFullYear()
-        }
-        var d = new Date(this.year, this.month + 1, 0);
-        console.log(d)
-        return d;
+        return 30;
       }
       if (word.toUpperCase() === 'FIRST') {
         return 1;
       }
-      return ordinals[word]
-    },
+      return this.ordinals[word]
+    }
     enumerateSecondWord(word) {
         if (word.toUpperCase() === 'DAY') {
             return days
@@ -96,23 +57,22 @@ let Calendar = (function() {
         if (word.toUpperCase() === 'MONTH') {
             return days
         }
-    },
+    }
     enumerateFourthWord(word) {
         var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
         if(format.test(word)){
           return true;
         } else {
-         for(let x in months) {
-             if (months[x].toUpperCase() === word.toUpperCase()) {
-                return months.indexOf(word.charAt(0).toUpperCase()+word.slice(1))+1;
+         for(let x in this.months) {
+             if (this.months[x].toUpperCase() === word.toUpperCase()) {
+                return this.months.indexOf(word.charAt(0).toUpperCase()+word.slice(1))+1;
              }
          }
         }
-    },
+    }
     enumerateFiftWord(word) {
         return word
-    },
-
+    }
     getDaysInMonth(month, year) {
       this.month = month;
       this.year = year;
@@ -124,7 +84,7 @@ let Calendar = (function() {
         date.setDate(date.getDate() + 1);
       }
       return days;
-    },
+    }
     getAllDateInMonth(month, year) {
       let days = this.getDaysInMonth(month, year);
       let result = [];
@@ -132,17 +92,16 @@ let Calendar = (function() {
         result.push(days[day].toDateString());
       }
       return result;
-    },
+    }
     getDay(month, day, year) {
-      date = new Date(year, month-1, day);
-      return months[date.getMonth()] + ' '+ day + ' ' + year;
-    },
+      let date = new Date(year, month-1, day);
+      return this.months[date.getMonth()] + ' '+ day + ' ' + year;
+    }
     getMonth() {
 
-    },
+    }
     getDayString() {
         string = date.toString().split(' ')
         return days[string[0]]
-    },
-  }
-})()
+    }
+}
